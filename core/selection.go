df2fcd65597d60d2dedd3a48c4754896139f1193
D Souza <stedsou@capel0058153215.deefs.corpintra.net>
2021-04-07 22:44:23 +0200

    Refactored code

--- core/selection.go
+++ core/selection.go
@@ -4,16 +4,15 @@ import (
 	"math/rand"
 )
 
-func ComputerSelection() string {
-	/* FIXME Sometimes it is blank */
-	/* rand.Seed(time.Now().UnixNano())  https://golang.cafe/blog/golang-random-number-generator.html */
-
+// GetComputerSelection : Computer's selection taken from the game option dict.
+func GetComputerSelection() string {
 	randomIndex := rand.Intn(len(GameOptions))
 	computerPick := GameOptions[randomIndex]
 	return computerPick
 }
 
-func PlayerSelection(playerInput int) string {
+// GetPlayerSelection : Player's selection taken from the game option dict.
+func GetPlayerSelection(playerInput int) string {
 	playerPick := GameOptions[playerInput]
 	return playerPick
 }
